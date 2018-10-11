060aff4ae5bf05d068db1ea6ac3d443cec536178
Meuven <43167875+Meuven@users.noreply.github.com>
2018-10-11 15:22:03 +0200

    Update rotor.py

--- rotor.py
+++ rotor.py
@@ -18,20 +18,23 @@ import time
 import os 
 from time import sleep
 
+# get the Comm Port information
+timeout = ?
+port = ?
+baud = ?
 
-cmd = [0]*13                   # command packet get 13 byte,  ASCII characters for az and el
-cmd[0] = 0x57                  # S (start) shall always be 0x57 = 87
-cmd[12] = 0x20                 # End                    be 0x20 = 32
+#Open Comm Port
+ser = serial.Serial(port, baud, timeout = 0)
 
 rep = ['0']*12                    # reponse packet get 12 byte, Byte values for az and el
-rep[0] = 'W'
-rep[11] = ' '
+rep[0] = 'W'			  # ASCII charac for 0x57
+rep[11] = ' '			  # ASCII charac for 0x20
 
 
-azi_max = 360
-azi_min = 0
-ele_max = 180 
-ele_min = 0 
+az_max = 360
+az_min = 0
+el_max = 180 
+el_min = 0 
 loop = 1
 zero5 = chr(0)+chr(0)+chr(0)+chr(0)+chr(0)             # this variable will serve to give the commande to the driver 
 
@@ -43,56 +46,108 @@ zero5 = chr(0)+chr(0)+chr(0)+chr(0)+chr(0)             # this variable will serv
 # As I don't really know the port and input yet, 
 #we'r going to work with the liste commande and reponse as our intput as if they contained our datas.
 
-# get the Comm Port information
-#input_variable = raw_input ("Enter comm port: ")
-#port = (input_variable)
-#input_variable = input("Enter comm port baud rate: ")
-#baud = (input_variable)
 
+print ("Enter 888 to stop rotation.")
+print ("Enter 999 to update rotator status.")
+print ("Enter 987 to quit program.")
 
-
-
-print ("Enter stop to stop rotation.")
-print ("Enter status to update rotator status.")
-print ("Enter quit  to quit program.")
-#Get desired azimuth
+#Get desired az and mode at the same time.
    print (" ")
-   input_variable = input ("Enter mode:")
-   mode = str(input_variable)
+   input_az = input ("Enter azimuth: ")
+   az = int(input_az)
+   
 
 loop = 1
 while loop == 1 :
-  if mode == "stop" :               # in this mode we just stop the rotator and have back the az and el 
-    output = chr(87)+zero5+zero5+chr(15)+chr(32)
-    cmd = open('data.txt', 'a')     # the format.txt is not the good one 
-    cmd.write(output)
-    cmd.close
-    sleep(1)                        # wait for an answer 
-      
-#Now it's time to give the réponse to the user, before, we need to convert our datas:
-    H1 = ord(rep[1])
-    H2 = ord(rep[2])
-    H3 = ord(rep[3])
-    H4 = ord(rep[4])
-    az_res = ord(rep[5])
-    V1 = ord(rep[6])
-    V2 = ord(rep[7])
-    V3 = ord(rep[8])
-    V4 = ord(rep[9])
-    el_res = ord(rep[10])
+	if mode == 997 :                  # in this mode we just stop the rotator and have back the az and el 
+		output = chr(87)+zero5+zero5+chr(15)+chr(32) # this cmd will stop the rotator
+	    	cmd= ser.write(output)
+    		sleep(1)                        # wait for an answer
+
+    		rep = ser.read(12)
+#Now it's time to give the reponse to the user, before, we need to convert our datas:
+    		H1 = ord(rep[1])
+    		H2 = ord(rep[2])
+    		H3 = ord(rep[3])
+    		H4 = ord(rep[4])
+    		az_res = ord(rep[5])
+    		V1 = ord(rep[6])
+    		V2 = ord(rep[7])
+    		V3 = ord(rep[8])
+    		V4 = ord(rep[9])
+    		el_res = ord(rep[10])
   
-    azs = H1*100 + H2*10 + H3 + H4/10
-    els = V1*100 + V2*10 + V3 + V4/10
-	 # Since the controller sends the status based on 0 degrees = 360
-         # remove the 360 here
-    azs = azs - 360
-    els = els - 360
-    print ("Rotator stopped at %3d " %(azs)+ "Degrees Azimuth and %3d " %(els) + "Degrees Elevation")
-    print ("Azimuth multiplier is %3d "%(az_res)+ "  Elevation Multiplier is %3d "%(el_res)
+    		azs = H1*100 + H2*10 + H3 + H4/10
+    		els = V1*100 + V2*10 + V3 + V4/10
+	 	# Since the controller sends the status based on 0 degrees = 360
+         	# remove the 360 here
+    		azs = azs - 360
+    		els = els - 360
+    		print ("Rotator stopped at %3d " %(azs)+ "Degrees Azimuth and %3d " %(els) + "Degrees Elevation")
+    		print ("Azimuth multiplier is %3d "%(az_res)+ "  Elevation Multiplier is %3d "%(el_res))
       
       
-               
-      
+	elif az == 998:
+    		# Build the status command word.
+    		output = chr(87)+zero5+zero5+chr(31)+chr(32)
+    		cmd= ser.write(output)
+    		sleep(1) 
+
+    		rep = ser.read(12)
+		# Now as the stop commande, we're going to give the reponse to the user with more detail, but the rotator is still running
+    		H1 = ord(rep[1])
+    		H2 = ord(rep[2])
+    		H3 = ord(rep[3])
+    		H4 = ord(rep[4])
+    		az_res = ord(rep[5])
+    		V1 = ord(rep[6])
+    		V2 = ord(rep[7])
+    		V3 = ord(rep[8])
+    		V4 = ord(rep[9])
+    		el_res = ord(rep[10])
+  
+    		azs = H1*100 + H2*10 + H3 + H4/10
+    		els = V1*100 + V2*10 + V3 + V4/10
+	 	# Since the controller sends the status based on 0 degrees = 360
+         	# remove the 360 here
+    		azs = azs - 360
+    		els = els - 360
+    		print ("Rotator stopped at %3d " %(azs)+ "Degrees Azimuth and %3d " %(els) + "Degrees Elevation")
+    		print ("Azimuth multiplier is %3d "%(az_res)+ "  Elevation Multiplier is %3d "%(el_res))
+
+	elif  az == 989 :
+		loop = 0 
+
+ 	else :
+# now we have the az, we are not in the 3 other modes, so we are automatically in the "set mode",
+# so we ask the el to the rotator execute the commande.
+		az = az %360
+   		input_el = input("Enter Elevation: ")
+           	el = int(input_variable)%180
+		
+		#see the protocol 
+		az = az + 360
+                el = el + 360
+                
+		multi = 1 		# multi will be fixed according to the accuracy we need 
+              	az = az * multi
+              	el = el * multi
+
+              azm = str(az)
+              elm = str(el)
+              if len(azm) == 3:
+                 azm = "0" + azm
+              if len(elm) == 3:
+                 elm = "0" + elm
+  
+              # Build message to be sent to controller
+	      output =  chr(87) + azm + chr(multi)+elm + chr(multi)+chr(47)+chr(32) # this cmd will stop the rotator
+	      cmd= ser.write(output)
+    	      sleep(1)                        # wait for an answer
+	      
+	     
+
+
   
 
 
