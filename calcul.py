5cbec5ee4ba49440c1c500e6ae72c6b0a0074b14
Meuven <43167875+Meuven@users.noreply.github.com>
2018-10-17 17:25:54 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -11,50 +11,43 @@ import matplotlab.py as plt
 from plt import *
 import ephem
 import serial 
+from time import sleep
 
-def calcul ():
+#class calcul:
+    #time sleep = 0.1
+    #def __init__(self, ip, port,baud,timeout, time_to_stop=None, frequency=None, leep_time=None):
+        #self.IP = ip 
+        #self.PORT = port 
+        #self.BAUD = baud 
+        #self.TIMEOUT = timeout 
+        
 
+
+        
+        
+ #we are going to first decoded the tle. The information we're are going to extract, is the time when the 
+# sat will be in the area, and it coordonates .
+( reprendre net R9 page 77 )
+        
+        
+        
+        
 #Open port for the NetR9 GPS 
+ser = serial.Serial(port, baud, timeout)
 
-  port = ?
-  baud = ?
-  timeout = ?
-
-  ser  = serial.Serial(port, baud, timeout)
-
-
-  commande = [0]*13                   # command packet get 13 byte,  ASCII characters for az and el
-  commande[0] = 0x57                  # S (start) shall always be 0x57
-  commande[12] = 0x20                 # End                    be 0x20 
-
-reponse = [0]*12                    # reponse packet get 12 byte, Byte values for az and el
-reponse[0] = 0x57
-reponse[11] = 0x20
-reponse[5] = 0x01                   # We gonna put for the moment resolution to 1deg/pulse
-reponse[10] = reponse[5]            # so we put the resolution to 1
-
-shipaz = [0]*4                      # Here we'll be save the orientation of the ship, ASCII characters for az 
-                                    # note that  the ship doesn't have El orientation
-
-  
-                                    # Note that the netR9 has an HDT command which gives us directly the orientation of the sbt based on 
-                                    # the geo north. The idea here is to save every last orientation to predict the orientation of the sbt
-                                    # when the mission will start.
-  
-def init(T) :                       # T is the time of the mission,
-  gps= open("gps.txt","a")
-  Lgps = open("gps.txt","a")
-  for i in range(int(T)+1) :        # T could be a float #
-    time.sleep(1)
-    gps.write("lat, long, d° /n")   # datas will before of all to be decoded in RINEX fortmat 
-    Lgps.write("lat, long, d° /n")
-  Lgps.split("/n")
-  while Len(Lgps) > 10 :
-    del(Lgps[0])
-  return(Lgps)
-
-L = init(T)
-
+# Note that the netR9 has an HDT command which gives us directly the orientation of the sbt based on the geo north.
+    loop = 1
+    while loop ==1:
+        output = ?
+        cmd = ser.write(output)
+        sleep(0,1)              # wait for an answer
+        
+        if ser.avaible() ==0 :  # if not answer wait the time T to have a new coordonate of the w
+            sleep(T)
+        else:
+            ship = ser.read(1000)
+            loop = 0
+            ser.close()
      
 
         
