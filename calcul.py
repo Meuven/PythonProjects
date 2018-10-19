3635ee8dd22b1499dd872535aee87b7a4d334437
Meuven <43167875+Meuven@users.noreply.github.com>
2018-10-19 11:47:53 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -1,1 +1,6 @@
+# now we get our sat and ship coordonate, we can calcul the az and el for the rotor .
+# this code should be easy and fast to calculate the command for the rotor every seconde .
 
+def calcul(deg, lat_lg,sat):
+    
+    # we first are going to calculate the az which is easier because it only depends on the ship's orientation .
