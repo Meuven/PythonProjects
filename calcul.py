c86765f31d88f1659b625304715cb68bcdbe72e8
Meuven <43167875+Meuven@users.noreply.github.com>
2018-10-19 14:30:27 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -3,4 +3,6 @@
 
 def calcul(deg, lat_lg,sat):
     
-    # we first are going to calculate the az which is easier because it only depends on the ship's orientation .
+# we first are going to calculate the az which is easier because it only depends on the ship's orientation .
+# in the first time we aregoing to calculate the distance between two points on a map 
+# el = arctan( alt/dis) dis to determinate by the lat and long 
