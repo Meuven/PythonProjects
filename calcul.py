a22a5d965e6aabf6c4f3c544b55d3e39b9b7af43
Meuven <43167875+Meuven@users.noreply.github.com>
2018-10-22 17:12:14 +0200

    Update calcul.py

--- calcul.py
+++ calcul.py
@@ -3,12 +3,21 @@
 
 def calcul(deg, lat_lg,sat):
     
-# we first are going to calculate the az which is easier because it only depends on the ship's orientation .
+
 # in the first time we aregoing to calculate the distance between two points on a map 
 # el = arctan( alt/dis) dis to determinate by the lat and long 
-# two modules were find to calculate the distance between 2 point of the map .
-# The first one is Geopy  and the seconde one est Mpu.
+
+#The lyb I found to calculate the distance between 2 point of the map : latlon .
+# We can fin here the different functions of the lyb :https://pypi.org/project/LatLon/#description
 
 #note : ephem gives the lat and long coor on deg min sec . This one should be convert first in deg decimal for the calcul
-# see ephem angle to convert 
+#note : the converstion could be do with the cal for a lat for example (lat = d.m.s), lat_dec = d+(m/60)+(s/3600)
+
+lat_sat = 
+
+
+
+
+
+
 # note : GNSS give the lat and long coor on  deg decimal . it's ok  for the calcul .
